8ff9189ce9c310ac50bc99d1646449731dd77c18
moonnnluv <ale.reyesd@duocuc.cl>
2025-05-13 15:44:29 -0400

    10.Hibernate y JPA-2

--- springboot-jpa/src/main/java/com/ale/springboot/jpa/springboot_jpa/SpringbootJpaApplication.java
+++ springboot-jpa/src/main/java/com/ale/springboot/jpa/springboot_jpa/SpringbootJpaApplication.java
@@ -1,4 +1,6 @@
 package com.ale.springboot.jpa.springboot_jpa;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.CommandLineRunner;
@@ -6,6 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.ale.springboot.jpa.springboot_jpa.entities.Person;
 import com.ale.springboot.jpa.springboot_jpa.repositories.PersonRepository;
+
 
 
 @SpringBootApplication
@@ -20,7 +23,18 @@ public class SpringbootJpaApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception{
+		List<Person> persons = (List<Person>) repository.buscarLenguajeyNombre("Python", "Ale");
+		persons.stream().forEach(person ->{
+			System.out.println(persons);
+		});
 
+
+	}
+
+	public void crear(){
+		Person person = new Person(null,"Emilia", "Munoz", "Java");
+		Person personNew = repository.save(person);
+		System.out.println(personNew);
 	}
 
 }
--- springboot-jpa/src/main/java/com/ale/springboot/jpa/springboot_jpa/repositories/PersonRepository.java
+++ springboot-jpa/src/main/java/com/ale/springboot/jpa/springboot_jpa/repositories/PersonRepository.java
@@ -1,9 +1,17 @@
 package com.ale.springboot.jpa.springboot_jpa.repositories;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import com.ale.springboot.jpa.springboot_jpa.entities.Person;
+import java.util.List;
+
 
 public interface PersonRepository extends CrudRepository<Person, Long> {
+    List<Person> findByLenguajes(String lenguajes);
+
+    @Query("select p from Person p where p.Lenguajes=?1 and p.nombre=?2")
+    List<Person> buscarLenguajeyNombre(String lenguajes, String nombre);
+
 
     
 }
